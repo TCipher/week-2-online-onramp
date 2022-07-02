fadaaf1a173e6023040d34df9fd4eeb11eaeb0fc
Tochukwu <tochind29@gmail.com>
2022-07-02 10:22:00 +0100

    main1

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,3 +1,10 @@
-function missingNumber(numArr) {}
+function missingNumber(numArr) {
+    const num = [1,2,3,4,5,6,7,8,9]
+    let result = num.filter(x => !numArr.includes(x))
+        if(result.length === 1){
+            re
+        }
+    }
+
 
 module.exports = missingNumber
